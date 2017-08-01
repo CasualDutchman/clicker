8d15c4eb3c07ff5b85f9ab8f78feac726026a1c0
Pieter <pieterj@live.nl>
2017-08-01 03:11:32 +0200

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.Analytics;
+
+public class GameManager : MonoBehaviour {
+
+    public int level = 1;
+
+    public GameType type;
+
+    public bool chaosMode;
+
+    public Transform slider;
+    public Text objectiveText, maxLevelText;
+    public Image timerImage;
+    Button[] buttons = new Button[9];
+    int[] buttonValue = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int[] objective;
+    int nextIndex = 0;
+
+    int maxNumbersAchieved;
+
+    float timer;
+    float maxTimer = 1.5f;
+
+	void Start () {
+        Transform holdingButtons = GameObject.FindObjectOfType<GridLayoutGroup>().transform;
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = holdingButtons.GetChild(i).GetComponent<Button>();
+            int index = i;
+            buttons[i].onClick.AddListener(() => HitButton(index));
+        }
+
+        Load();
+
+        NewNumber(StartType.Restart);
+    }
+	
+    void Load() {
+        maxNumbersAchieved = PlayerPrefs.GetInt("HighScore");
+        maxLevelText.text = maxNumbersAchieved.ToString();
+    }
+
+    void Save() {
+        PlayerPrefs.SetInt("HighScore", maxNumbersAchieved);
+        PlayerPrefs.Save();
+    }
+
+    void NewNumber(StartType newLevel) {
+        nextIndex = 0;
+        timer = 0;
+        if (newLevel == StartType.AddOne) {
+            level += 1;
+            maxTimer += 0.5f;
+            if(level > maxNumbersAchieved)
+                maxNumbersAchieved = level;
+            maxLevelText.text = maxNumbersAchieved.ToString();
+        } 
+        else if (newLevel == StartType.RemoveOne) {
+            level = Mathf.Clamp(level - 1, 1, int.MaxValue);
+            maxTimer = Mathf.Clamp(maxTimer - 0.5f, 1.5f, float.MaxValue);
+        } 
+        else if(newLevel == StartType.Restart) {
+            level = 1;
+            maxTimer = 1.5f;
+        }
+
+        if (chaosMode) {
+            changeButtonValue(true);
+        }
+
+        objectiveText.text = GetRandomNumber();
+    }
+
+    string GetRandomNumber() {
+        string number = "";
+        objective = new int[level];
+        for (int i = 0; i < objective.Length; i++) {
+            objective[i] = Random.Range(1, 9);
+            number += objective[i];
+        }
+        return number;
+    }
+
+    void changeButtonValue(bool change) {
+        for (int i = 0; i < 9; i++) {
+            int temp = 0;
+            if (change) {
+                int swapper = Random.Range(0, 9);
+                temp = buttonValue[swapper];
+                buttonValue[swapper] = buttonValue[i];
+            }
+            buttonValue[i] = change ? temp : i + 1;
+        }
+        for (int i = 0; i < 9; i++) {
+        Text text = buttons[i].transform.GetChild(0).GetComponent<Text>();
+        text.text = buttonValue[i].ToString();
+        }
+    }
+
+    void HitButton(int i) {
+        if (objective[nextIndex] == buttonValue[i]) {
+            nextIndex++;
+
+            string temp = "<color=#00ff00ff>";
+            for (int j = 0; j < nextIndex; j++) {
+                temp += objective[j];
+            }
+            temp += "</color>";
+            for (int k = nextIndex; k < objective.Length; k++) {
+                temp += objective[k];
+            }
+            objectiveText.text = temp;
+            
+            if (nextIndex >= level) {
+                NewNumber(StartType.AddOne);
+                return;
+            }
+        } else {
+            NewNumber(StartType.RemoveOne);
+        }
+    }
+
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape)) {
+            if (type == GameType.Menu) {
+                Save();
+                Application.Quit();
+            }
+            else if (type == GameType.Game) {
+                type = GameType.ToMenu;
+            }
+        }
+
+        if (type == GameType.Game) {
+            timer += Time.deltaTime;
+
+            timerImage.fillAmount = 1 - (timer / maxTimer);
+
+            if (timer >= maxTimer) {
+                NewNumber(StartType.RemoveOne);
+            }
+        }
+        else if (type == GameType.ToGame) {
+            timer += Time.deltaTime * 300;
+            slider.localPosition = Vector3.Lerp(Vector3.zero, new Vector3(-400, 0, 0), timer / 400);
+            if (slider.localPosition.x <= -400) {
+                slider.localPosition = new Vector3(-400, 0, 0);
+                timer = 0;
+                type = GameType.Game;
+            }
+        } else if (type == GameType.ToMenu) {
+            timer += Time.deltaTime * 300;
+            slider.localPosition = Vector3.Lerp(new Vector3(-400, 0, 0), Vector3.zero, timer / 400);
+            if (slider.localPosition.x >= 0) {
+                slider.localPosition = Vector3.zero;
+                timer = 0;
+                type = GameType.Menu;
+            }
+        }
+    }
+
+    public void HitStart() {
+        type = GameType.ToGame;
+        NewNumber(StartType.Restart);
+        timerImage.fillAmount = 1;
+        if (!chaosMode)
+            changeButtonValue(false);
+
+        Analytics.SetUserGender(Gender.Female);
+        Analytics.SetUserBirthYear(1995);
+    }
+
+    public void HitHome() {
+        type = GameType.ToMenu;
+    }
+
+    public void toggleChoasMode() {
+        chaosMode = !chaosMode;
+    }
+}
+
+public enum StartType {
+    Restart, AddOne, RemoveOne
+}
+
+public enum GameType {
+    Menu, Game, ToGame, ToMenu
+}
